08c47b282bc6d594d3340d77c1da4a31810c0507
Prakash Pandey <pcpandey@mail.com>
2018-07-11 15:07:27 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+extern crate rand;
+extern crate termion;
+
+use std::io;
+use std::io::Write;
+use rand::Rng;
+use termion::color;
+//use termion::clear;
+
+fn game(score: &mut i32, lower_limit: &i32, upper_limit: &i32) {
+	let secret_number = rand::thread_rng().gen_range(*lower_limit, *upper_limit);
+	
+	print!("Input your guess:\t\t\t");
+	io::stdout().flush().ok().expect("Could not flush stdio");
+	
+	let mut guess = String::new();
+
+	io::stdin().read_line(&mut guess)
+		.expect("Error reading line");
+
+	let guess: i32 = guess.trim().parse()
+		.expect("Please type a number");	
+	
+	print!("You guessed: {} \t\t\t\tThe secret number is: {} ", guess, secret_number);
+	
+	if guess == secret_number {
+		print!("{green}\t\tYou won{reset}",
+             		green   = color::Fg(color::Green),
+             		reset = color::Fg(color::Reset));
+		*score += 1;	
+		
+	} else {
+		print!("{red}\t\tYou lose{reset}",
+             		red   = color::Fg(color::Red),
+             		reset = color::Fg(color::Reset));	
+	}
+	
+}
+
+fn message(lower_limit: &i32, upper_limit: &i32, max_attempt: &i32) {
+	println!("\n-----------------------------------------------------------------------------------------------------------");
+	println!("Guess the number[{} - {}] \t\tmax attempt: {}", *lower_limit, *upper_limit, *max_attempt);
+}
+
+fn clear() {
+	std::process::Command::new("clear").status().unwrap().success();
+	//println!("{}", clear::All);
+}
+
+fn main() {
+	let upper_limit = 15;
+	let lower_limit = 0;
+
+	let mut score = 0;
+	let max_attempt: i32 = 5;
+	let mut i = 0;
+	
+	
+	while i<max_attempt {
+		clear();
+		message(&lower_limit, &upper_limit, &max_attempt);
+		println!("Attempt: {} \t\t\t\tScore: {}", i, score);
+		println!("\n");
+		game(&mut score, &lower_limit, &upper_limit);
+		i += 1;
+	}
+	clear();
+	println!("\n-------------------------------------------------------");
+	println!("|Total attempt: {} \t\t\tTotal score: {}|", i, score);
+	println!("-------------------------------------------------------");
+}
